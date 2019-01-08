f499e05b9890c3b5ed72816e486b63ed4531f859
Denis Babiichuk <den2512@ukr.net>
2019-01-08 17:33:14 +0200

    Update ciaoItalia

--- ciao-italia/send.php
+++ ciao-italia/send.php
@@ -1,31 +1,19 @@
-<?php
-if ($_POST) {
-        /*$to_email = "shevchuk@prof.estate"; //Recipient email, Replace with own email here*/
-        $to_email = "den2512@ukr.net"; //Recipient email, Replace with own email here
-        //Sanitize input data using PHP filter_var().
-        $user_name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-        $user_tel = filter_var($_POST["tel"], FILTER_SANITIZE_STRING);
-
-        // subject
-        $subject = "Ciao Italia";
-
-        //email body
-        $message_body = $user_name."\r\n".$user_tel."\r\n".$user_email."\r\n".$user_comment;
-
-        //proceed with PHP email.
-        $headers = 'From: '.$user_name.'' . "\r\n" .
-        'Reply-To: '.$user_phone.'' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-        $send_mail = mail($to_email, $subject, $message_body, $headers);
-
-        if (!$send_mail){
-            //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
-            $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
-            die($output);
-        }else{
-            $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_name .' Thank you for your email'));
-            die($output);
-        }
-    }
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")){
+        $to = 'den2512@ukr.net';
+        $subject = 'Обратный звонок - Ciao Italia';
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['tel'].'</p>
+                    </body>
+                </html>';
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+        $headers .= "From: Отправитель <from@example.com>\r\n";
+        mail($to, $subject, $message, $headers);
+}
 ?>
